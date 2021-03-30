4a0320b8cc7fc2b8c27e6d3642ba9dc56f34637d
Allan Tenenbaum <53152572+astronut-og@users.noreply.github.com>
2021-03-29 22:49:52 -0400

    automatic playlist support, fixed some issues.

--- main.py
+++ main.py
@@ -5,20 +5,54 @@ import moviepy
 import shutil
 import eyed3
 import os
-
+import mutagen
 from pytube import YouTube
+from mutagen.easyid3 import EasyID3
+from pytube import Playlist
 
-user_link = input("Enter the link: ")
-yt = YouTube(user_link)
 
-def get_video_title():
-    title = yt.title
-    return title
+def get_user_input():
+    user_input = input("Enter the link: ")
+    if "playlist" in user_input:
+        playlist = Playlist(user_input)
+        print("This is a playlist, starting download")
+        for song in playlist.videos:
+            download_video(song)
+        print("Finished downloading playlist")
+    else:
+        youtube_obj =  YouTube(user_input)
+        download_video(youtube_obj)
 
-def get_thumbnail_image():
+def download_video(youtube_obj):
+    title = get_video_title(youtube_obj)
+    youtube_video = youtube_obj.streams.filter(progressive=True).get_highest_resolution()
+    youtube_video.download()
+    print("MP4 is downloaded")
+    convert_video_to_audio(title)
+    print("Converted the MP4 to MP3")
+    get_thumbnail_image(youtube_obj)
+    print("Thumbnail downloaded")
+    set_thumbnail(title)
+    print("Thumbnail has been set")
+    set_artist(youtube_obj, title)
+    print("Artist has been set")
+    cleanup(title)
+    cls()
+    array.append(f"{title} has been successfully downloaded")
+    for song in array:
+        print(song)
+
+
+def get_video_title(youtube_obj):
+    return youtube_obj.title
+
+def get_thumbnail_image(youtube_obj):
     """ this downloads the thumbnail of the video to later set as the thumbnail of the mp3
     """
-    thumbnail_url = yt.thumbnail_url
+    thumbnail_url = youtube_obj.thumbnail_url
+    print(thumbnail_url)
+    thumbnail_url = thumbnail_url.split("?")[0]
+    print(thumbnail_url)
     # sends request to get the thumbnail from the video
     r = requests.get(thumbnail_url, stream=True)
     filename = thumbnail_url.split("/")[-1]
@@ -34,35 +68,43 @@ def get_thumbnail_image():
     else:
         print('Image Couldn\'t be retreived')
 
-def convert_video_to_audio():
+def convert_video_to_audio(filename):
     """ this converts the mp4 file you just downloaded to an mp3 file
     """
-    mp4_title = get_video_title() + ".mp4"
-    mp3_title = get_video_title() + ".mp3"
+    mp4_title = filename + ".mp4"
+    mp3_title = filename + ".mp3"
     videoclip = VideoFileClip(mp4_title)
     audioclip = videoclip.audio
     audioclip.write_audiofile(mp3_title)
     audioclip.close()
     videoclip.close()
 
-def set_thumbnail():
+def set_thumbnail(filename):
     """ this sets a thumbnail for the mp3 which is just the thumbnail from the youtube video
     """
-    audiofile = eyed3.load(get_video_title() + ".mp3")
+    mp3_url = filename + ".mp3"
+    audiofile = eyed3.load(mp3_url)
     if(audiofile.tag == None):
         audiofile.initTag()
     audiofile.tag.images.set(3, open('maxresdefault.jpg', 'rb').read(), 'image/jpeg')
     audiofile.tag.save()
 
-def cleanup():
+def set_artist(youtube_obj, filename):
+    audio = EasyID3(filename + ".mp3")
+    artist_name = youtube_obj.author
+    audio['artist'] = f"{artist_name}"
+    audio.save()
+
+def cleanup(filename):
     """ this deletes the now-useless files
     """
-    os.remove(get_video_title() + ".mp4")
+    os.remove(filename + ".mp4")
     os.remove("maxresdefault.jpg")
 
-ys = yt.streams.filter(progressive=True).get_highest_resolution()
-ys.download()
-convert_video_to_audio()
-get_thumbnail_image()
-set_thumbnail()
-cleanup()
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+status = True
+array = []
+while status:
+    get_user_input()
